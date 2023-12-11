b4a25006ea4f7c199dc84e79792b0fe617f0af0e
Osttik <lololka23@gmail.com>
2023-12-11 10:11:56 +0200

    Add respoce model

---
+++ MessageBus/Messages/ResponceOrderMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoServices.Shared.MessageBus.Messages
+{
+    public class ResponceOrderMessage
+    {
+        public Guid OrderId { get; set; }
+        public string State { get; set; }
+    }
+}
